1e1394d1ade0d003e3ce4fceb7b140d4570570b8
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2022-02-03 14:48:55 +0500

    start wroking on gui of the notepad

--- Notepad.java
+++ Notepad.java
@@ -1,6 +1,18 @@
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 class Notepad{
+	private JFrame window = new JFrame();
+	
 	public Notepad(){
+		window.setBounds(300,100,800,600);
+		window.setLayout(new BorderLayout(5,5));
 		
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args){
+		new Notepad();
 	}
 }
